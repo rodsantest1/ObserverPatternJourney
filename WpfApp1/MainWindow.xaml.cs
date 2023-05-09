44d1f6a0ed52102cffbfddb829fd5a940ad3a840
rodneysantiago <rodneysantiago@RSDEVPC1>
2023-05-08 21:39:23 -0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer = new();
+            customer.CustomerId = 100;
+            MessageBox.Show($"Customer {customer.CustomerId} added!");
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; } 
+
+    }
+}
